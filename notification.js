37599c38abaa41de0f80c2534856f7240b115661
flamenx01 <flamenx01@gmail.com>
2017-09-21 19:07:38 +0100

    Created basic command line parser for the file
    Created basic command line parser for the file. Takes the file and prints the relevent message.

--- notification.js
+++ notification.js
@@ -8,9 +8,9 @@ for(i=0;i<json.length;i++)
 		console.log(json[i].data.from_name + " replied to you in "+ json[i].data.subject );		
 	}
 	else if(json[i].type == "gallery"){
-		console.log(json[i].data.from_name + " replied to you in "+ json[i].data.subject );				
+		console.log(json[i].user_name + " uploaded new file to gallery "+ json[i].data.gallery_title);				
 	}
 	else if(json[i].type == "file"){
-		
+		console.log(json[i].data.user_name + " uploaded new file "+ json[i].data.file_name + " to "+ json[i].data.module_name);						
 	}
 }
