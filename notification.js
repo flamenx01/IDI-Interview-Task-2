937c7ed27f098280a3b81050694941645e3eadd2
flamenx01 <flamenx01@gmail.com>
2017-09-21 19:20:26 +0100

    Added time since post to console parser
    Now displays time (year/month/week/ etc) since a the post was made.

--- notification.js
+++ notification.js
@@ -1,16 +1,48 @@
+function timeSince(dateIn) {
+
+	var date = new Date(dateIn);
+
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+
+
+
 var json = require('./data.json');	
 for(i=0;i<json.length;i++)
 {
 	if (json[i].type == "message"){	
-		console.log(json[i].data.from_name + " messaged you in "+ json[i].data.subject );
+		console.log(timeSince(json[i].created_at)+" @ "+json[i].data.from_name + " messaged you in "+ json[i].data.subject );
 	}
 	else if(json[i].type == "forum"){
-		console.log(json[i].data.from_name + " replied to you in "+ json[i].data.subject );		
+		console.log(timeSince(json[i].created_at)+" @ "+json[i].data.from_name + " replied to you in "+ json[i].data.subject );		
 	}
 	else if(json[i].type == "gallery"){
-		console.log(json[i].user_name + " uploaded new file to gallery "+ json[i].data.gallery_title);				
+		console.log(timeSince(json[i].created_at)+" @ "+json[i].user_name + " uploaded new file to gallery "+ json[i].data.gallery_title);				
 	}
 	else if(json[i].type == "file"){
-		console.log(json[i].data.user_name + " uploaded new file "+ json[i].data.file_name + " to "+ json[i].data.module_name);						
+		console.log(timeSince(json[i].created_at)+" @ "+json[i].data.user_name + " uploaded new file "+ json[i].data.file_name + " to "+ json[i].data.module_name);						
 	}
 }
